9a4487083a974ca4449eac4d8d6bde7c7762c552
adhamito <aalmechkor2@gmail.com>
2024-11-22 13:14:30 +0100

    Update layout.tsx for improved structure and Clerk integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
-import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
-
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
+import { Metadata } from "next";
 
-const IBMPlex = IBM_Plex_Sans({
+const IBM_Plex_Sans_Font = IBM_Plex_Sans({
   subsets: ["latin"],
   weight: ["400", "500", "600", "700"],
   variable: "--font-ibm-plex",
@@ -21,10 +21,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-        {children}
-      </body>
-    </html>
+    <ClerkProvider
+      appearance={{
+        variables: {
+          colorPrimary: "#624cf5",
+        },
+      }}
+    >
+      <html lang="en">
+        <body
+          className={cn(
+            "font-IBM_Plex_Sans antialiased",
+            IBM_Plex_Sans_Font.variable
+          )}
+        >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
