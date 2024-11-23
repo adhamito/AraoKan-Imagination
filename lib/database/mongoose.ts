526662127073edc1b62390b87003b4385a2776b7
adhamito <aalmechkor2@gmail.com>
2024-11-23 21:12:16 +0100

    Fix database path, update utilities, and cleanup model files

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -7,28 +7,35 @@ interface MongooseConnection {
   promise: Promise<Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose;
+// Use `let` or `const` appropriately
+let cached: MongooseConnection = (
+  global as unknown as { mongoose?: MongooseConnection }
+).mongoose || {
+  conn: null,
+  promise: null,
+};
 
-if (!cached) {
-  cached = (global as any).mongoose = {
+if (!cached.conn) {
+  cached = (global as unknown as { mongoose: MongooseConnection }).mongoose = {
     conn: null,
     promise: null,
   };
 }
 
-export const connectToDatabase = async () => {
+export const connectToDatabase = async (): Promise<Mongoose> => {
   if (cached.conn) return cached.conn;
 
   if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
 
-  cached.promise =
+  const promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
       dbName: "AraOkan",
       bufferCommands: false,
     });
 
-  cached.conn = await cached.promise;
+  cached.promise = promise;
+  cached.conn = await promise;
 
   return cached.conn;
 };
