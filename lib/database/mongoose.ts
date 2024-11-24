77e095ef25f8d6360d69c0d96c30827840c76d80
adhamito <100098044+adhamito@users.noreply.github.com>
2024-11-24 02:02:48 +0100

    Update mongoose.ts

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,41 +1,34 @@
-"use server";
 import mongoose, { Mongoose } from "mongoose";
 
 const MONGODB_URL = process.env.MONGODB_URL;
-
-if (!MONGODB_URL) {
-  throw new Error("Environment variable MONGODB_URL is not defined");
-}
 
 interface MongooseConnection {
   conn: Mongoose | null;
   promise: Promise<Mongoose> | null;
 }
 
-declare global {
-  // Using 'let' instead of 'var' for global variable declarations
-  let mongoose: MongooseConnection | undefined;
+let cached: MongooseConnection = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = {
+    conn: null,
+    promise: null,
+  };
 }
 
-const cached: MongooseConnection = global.mongoose || {
-  conn: null,
-  promise: null,
-};
+export const connectToDatabase = async () => {
+  if (cached.conn) return cached.conn;
 
-global.mongoose = cached;
+  if (!MONGODB_URL) throw new Error("Missing MONGODB_URL");
 
-export const connectionToDatabase = async (): Promise<Mongoose> => {
-  if (cached.conn) {
-    return cached.conn;
-  }
-
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URL, {
-      dbName: "AraOkan",
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: "imaginify",
       bufferCommands: false,
     });
-  }
 
   cached.conn = await cached.promise;
+
   return cached.conn;
 };
